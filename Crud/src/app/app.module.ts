d7453486e70a9823252002a0ff22462e87307cd8
¨R.carvalho¨ <ruby.bandeira@ifam.edu.br>
2023-07-25 12:34:33 -0500

    Atualizando repo

--- Crud/src/app/app.module.ts
+++ Crud/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './create/registration/registration.component';
+import { ListComponent } from './registration/list/list.component';
+import { DetailComponent } from './user/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
