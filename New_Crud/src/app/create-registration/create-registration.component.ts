2aa2b66b34d4cc3cfc17fa350af62cd1bf727f38
¨R.carvalho¨ <ruby.bandeira@ifam.edu.br>
2023-07-27 18:33:53 -0500

    Criando novas funcoes da aplicacao

---
+++ New_Crud/src/app/create-registration/create-registration.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-registration',
+  templateUrl: './create-registration.component.html',
+  styleUrls: ['./create-registration.component.scss']
+})
+export class CreateRegistrationComponent implements OnInit{
+  
+  public packages: string[] = ["Monthy","Quartely","Yarley"];
+  public genders: string[] = ["Male","Female"];
+  public importantList: string[] = [
+    "Toxic Fat reduction",
+    "Energy and Endurance",
+    "Building Lean Muscle",
+    "Healthier Digestive System",
+    "Sugar Craving Body",
+    "Fitness"
+  ];
+
+  public registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      mobile: [''],
+      weight: [''],
+      height: [''],
+      bmi: [''],
+      bmiResult: [''],
+      gender: [''],
+      requireTrainer: [''],
+      package: [''],
+      important: [''],
+      haveGymBefore: [''],
+      enquiryDate: ['']
+    }
+    )
+  }
+
+}
